c3524faa5a04243061e8e000e7e52f75789d9408
parthasarathy ak <parthasarathy3737@gmail.com>
2026-04-20 11:14:09 +0530

    fix: Current user cannot acces the other users url

--- src/main/java/com/sparnix/url_shortner/domain/services/ShortUrlService.java
+++ src/main/java/com/sparnix/url_shortner/domain/services/ShortUrlService.java
@@ -129,7 +129,8 @@ public class ShortUrlService {
         if(shortUrl.getExpiresAt() != null && shortUrl.getExpiresAt().isBefore(Instant.now())) {
             return Optional.empty();
         }
-        if(shortUrl.getIsPrivate() != null && shortUrl.getCreatedBy() != null
+        if(shortUrl.getIsPrivate() != null && shortUrl.getIsPrivate()
+                && shortUrl.getCreatedBy() != null
                 && !Objects.equals(shortUrl.getCreatedBy().getId(), userId)) {
             return Optional.empty();
         }
